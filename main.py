f8e4b8375eea6f539df82e43b146d51f6e0e7377
Falk <falk.kempe@protonmail.com>
2025-07-22 16:21:19 +0200

    added __main__

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from trade_setup import TradeSetup
 from chart import Chart
 
 
-def hello(name):
-    return f"Hallo {name}!"
+if __name__ == "__main__":
 
-chart = Chart("BTC/USDT", timeframe="5m")
-chart.fetch_data()
+    chart = Chart("BTC/USDT", timeframe="5m")
+    chart.fetch_data()
 
-chart.add_trade_box(
-    entry_time="2025-07-21 12:00",
-    entry_price=118200,
-    tp_price=117000,
-    sl_price=119000,
-    exit_time="2025-07-21 13:00"
-)
+    chart.add_trade_box(
+        entry_time="2025-07-21 12:00",
+        entry_price=118200,
+        tp_price=117000,
+        sl_price=119000,
+        exit_time="2025-07-21 13:00"
+    )
 
-chart.plot()
+    chart.plot()
