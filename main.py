c34b3eda18e345ceece54d3e1d1bf1528411f7d2
Falk <falk.kempe@protonmail.com>
2025-07-23 12:11:22 +0200

    added gradio

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import gradio as gr
-
 from trading_pair import TradingPair
 from trade_setup import TradeSetup
 from chart import Chart
 
 
-if __name__ == "__main__":
-
+def generate_plot():
     chart = Chart("BTC/USDT", timeframe="5m")
     chart.fetch_data()
-
     chart.add_trade_box(
         entry_time="2025-07-21 12:00",
         entry_price=118200,
@@ -17,5 +14,11 @@ if __name__ == "__main__":
         sl_price=119000,
         exit_time="2025-07-21 13:00"
     )
+    return chart.plot()  # Muss ein Matplotlib- oder Plotly-Figure sein
 
-    chart.plot()
+
+with gr.Blocks() as demo:
+    gr.Plot(generate_plot())
+
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=8080)
